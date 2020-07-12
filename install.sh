e5dd8b0965f435a7d53b6af4a02a30d48266fe37
Alex Villarreal <a@vill.co>
2020-07-12 14:32:46 +0000

    [vimrc] Comment commands and add preserve view

--- install.sh
+++ install.sh
@@ -15,13 +15,3 @@ for file in ${files}; do
 	ln -sf $HOME/dotfiles/home/${file} $HOME/.${file}
 done
 
-echo "--- Vundle ---"
-if [ -d $HOME/.vim/bundle/Vundle.vim ]; 
-then
-	echo "Vundle already installed"
-else
-	echo "Installing Vundle"
-	git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-	vim +PluginInstall +qall
-fi
-
